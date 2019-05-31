94cdc56a0ddb69ad161e4afec0214b31a7b52410
Alec Simpson <alecrsimpson@gmail.com>
2019-05-31 10:30:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,12 +4,12 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [
-  [0, 1, 2],  #top row across
-  [0, 3, 6],  #first column 
+  [0, 1, 2],  #top row 
+  [0, 3, 6],  #left column 
   [0, 4, 8],  #diagonal
   [1, 4, 7],  #middle column 
   [2, 4, 6],  #diagonal
-  [2, 5, 8],  #last column
+  [2, 5, 8],  #right column
   [3, 4, 5],  #middle row
   [6, 7, 8]   #bottom row
   ]
