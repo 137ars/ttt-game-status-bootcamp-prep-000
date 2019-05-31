4d51a2719482c72a32ad693d29dc8438b120f974
Alec Simpson <alecrsimpson@gmail.com>
2019-05-31 10:15:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,11 +15,22 @@ WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-  board.each do
-    return true
+  WIN_COMBINATIONS.each{|win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" 
+      return win_combination
   else
     return false
   end
 end
+}
+end
 
-# Define your WIN_COMBINATIONS constant
+
